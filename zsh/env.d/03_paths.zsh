e1b5d2620d5e5e9c3cd481b8804ec0079d2ab54f
Dave Nieuwenhuijzen <4242374+Ultimatum22@users.noreply.github.com>
2024-07-27 08:25:15 +0200

    Added miniconda to paths

--- zsh/env.d/03_paths.zsh
+++ zsh/env.d/03_paths.zsh
@@ -8,5 +8,20 @@ path=(/usr/local/bin /usr/local/sbin ${path})
 path=(${HOME}/.local/bin ${path})
 MANPATH="${XDG_DATA_HOME}/man:${MANPATH}"
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/dave/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/dave/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/dave/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/dave/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 # Add go binaries to paths
 path=(${GOPATH}/bin ${FLUTTER}/bin ${ANDROID_PLATFORM_TOOLS} ${path})
